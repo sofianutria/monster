83dc31db83b5a79ed4059a3aca4fec88c34cd8ad
Sofía Santos García <sofiasantos@MacBook-Air-de-Sofia.local>
2025-06-18 18:53:40 +0200

    feat: add crud features

--- src/main/java/com/example/Monster/dtos/MonsterMapper.java
+++ src/main/java/com/example/Monster/dtos/MonsterMapper.java
@@ -1,4 +1,27 @@
 package com.example.Monster.dtos;
 
+import com.example.Monster.models.Monster;
+
 public class MonsterMapper {
+    public static Monster toEntity(MonsterRequest request){
+        Monster monster = new Monster();
+        monster.setName (request.getName());
+        monster.setPrice (request.getPrice());
+        monster.setImageUrl (request.getImageUrl());
+        monster.setRating (request.getRating());
+        monster.setReviewCount (request.getReviewCount());
+        monster.setFeatured (request.isFeatured());
+        return monster;
+    }
+    public static MonsterResponse toResponse (Monster monster){
+        MonsterResponse response = new MonsterResponse();
+        response.setId(monster.getId());
+        response.setName (monster.getName());
+        response.setPrice (monster.getPrice());
+        response.setImageUrl (monster.getImageUrl());
+        response.setRating (monster.getRating());
+        response.setReviewCount (monster.getReviewCount());
+        response.setFeatured (monster.isFeatured());
+        return response;
+    }
 }
---
+++ src/main/java/com/example/Monster/models/Review.java
@@ -0,0 +1,4 @@
+package com.example.Monster.models;
+
+public class Review {
+}
